e907c7556c94909286d872b90c711226be4be36b
agabek <agabek309@gmail.com>
2022-06-17 10:27:41 +0600

    model init

--- app/Models/RolePermission.php
+++ app/Models/RolePermission.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class RolePermission extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['role_id','permission_id'];
+
+    function permission():BelongsTo
+    {
+        return  $this->belongsTo(Permission::class);
+    }
+    function role():BelongsTo
+    {
+        return $this->belongsTo(Role::class);
+    }
 }
