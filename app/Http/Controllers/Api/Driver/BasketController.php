a2f45fd889e1fa03d891c783a16bca7ceb79547c
agabek <agabek309@gmail.com>
2022-08-24 09:14:42 +0600

    initialState fix

--- app/Http/Controllers/Api/Driver/BasketController.php
+++ app/Http/Controllers/Api/Driver/BasketController.php
@@ -2,50 +2,47 @@
 
 namespace App\Http\Controllers\Api\Driver;
 
-use App\Actions\BasketCreateAction;
 use App\Actions\BasketUpdateAction;
 use App\Actions\OrderPriceAction;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Api\BasketStoreRequest;
 use App\Http\Requests\Api\BasketUpdateRequest;
-use App\Http\Requests\Api\OrderUpdateRequest;
 use App\Models\Basket;
 use App\Models\Order;
-use App\Models\OrderComment;
-use App\Models\PriceType;
-use App\Models\Product;
-use Barryvdh\DomPDF\Facade\Pdf;
-use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class BasketController extends Controller
 {
-    function index(Order $order):JsonResponse
+    function index(Order $order): JsonResponse
     {
         return response()->json($order->baskets()
             ->with('product')
-            ->join('products','products.id','baskets.product_id')
-            ->orderBy('products.measure','desc')
+            ->join('products', 'products.id', 'baskets.product_id')
+            ->orderBy('products.measure', 'desc')
             ->select('baskets.*')
             ->get()
         );
     }
-    function initialState(Order $order):JsonResponse
+
+    function initialState(Order $order): JsonResponse
     {
         foreach ($order->baskets as $basket) {
-            $firstBasket = $basket->audits()->whereEvent('created')->first()->new_values;
-            $basket->count = $firstBasket->count;
-            $basket->all_price = $firstBasket->all_price;
-            $basket->save();
+            $firstBasket = $basket->audits()->whereEvent('created')->first();
+
+            if ($firstBasket) {
+                $newValue = $firstBasket->new_values;
+                if (isset($newValue->count) and isset($newValue->all_priceK)) {
+                    $basket->count = $newValue->count;
+                    $basket->all_price = $newValue->all_price;
+                    $basket->save();
+                }
+            }
         }
         OrderPriceAction::execute($order);
 
-        return  response()->json(['message' => 'success']);
+        return response()->json(['message' => 'success']);
     }
 
-    function update(BasketUpdateRequest $request,Basket $basket)
+    function update(BasketUpdateRequest $request, Basket $basket)
     {
 
         $basket->count = $request->get('count');
@@ -56,6 +53,7 @@ class BasketController extends Controller
 
         return response()->json($basket);
     }
+
     function delete(Basket $basket)
     {
         $basket->delete();
